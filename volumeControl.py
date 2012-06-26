a37c9529762a620ee6fe323e5c58bc5b038febb4
Danny A. Barnes <barnes.dannya@gmail.com>
2012-06-26 10:26:22 -0400

    The python script that controls the LEDs, just sends 2 for now but will be changed to make system calls to change volume and send an appropriate response

---
+++ volumeControl.py
@@ -0,0 +1,14 @@
+import serial
+import time
+ser = serial.Serial()
+ser.baudrate = 9600
+ser.port = 2
+ser.open()
+while (True):
+    messageSignal = ser.read()
+    print(messageSignal)
+    if(messageSignal == B'D'):
+        distance = ser.readline()
+        print(distance)
+        ser.write('2'.encode('ascii'))
+ser.close()
